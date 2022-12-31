6ba26d075970d312e26fc3deb4bf3fa8a7f36fc2
MarielleCosta <marielle130costa@gmail.com>
2022-12-30 22:09:43 -0300

    Estudos java tipos e variaveis

--- java/java-anatomia-classes/src/edu/marielle/tiposEvariaveis/TiposVariaveis.java
+++ java/java-anatomia-classes/src/edu/marielle/tiposEvariaveis/TiposVariaveis.java
@@ -1,5 +1,18 @@
 package edu.marielle.tiposEvariaveis;
 
 public class TiposVariaveis {
-    
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+        System.out.print(numero);
+
+        final VALOR_DE_PI = 3.14;
+    }
 }
